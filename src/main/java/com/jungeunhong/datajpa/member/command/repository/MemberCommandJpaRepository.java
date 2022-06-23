703062ffbd4228b8bad960039f21891363bd24e3
hongjungeun <gjhong1129@gmail.com>
2022-06-23 10:48:37 +0900

    feat(/src/main/com/jungeunhong/datajpa/member/): 전통적인 방식의 페이징 쿼리 예시 추가

--- src/main/java/com/jungeunhong/datajpa/member/command/repository/MemberCommandJpaRepository.java
+++ src/main/java/com/jungeunhong/datajpa/member/command/repository/MemberCommandJpaRepository.java
@@ -30,4 +30,27 @@ public class MemberCommandJpaRepository {
                 .getResultList();
     }
 
+    // 전통적인 페이징 계산 방법
+    // totalPage = totalCount / size
+    // 마지막 페이지 인지판단 로직, 최초 페이지 판단로직 등에 사용된다.
+
+    public List<Member> findByPage(int age, int offset, int limit) {
+        return em.createQuery("select m " +
+                        "from Member m " +
+                        "where m.age = :age " +
+                        "order by m.username desc", Member.class)
+                .setParameter("age", age)
+                .setFirstResult(offset)
+                .setMaxResults(limit)
+                .getResultList();
+    }
+
+    public long totalCount(int age) {
+        return em.createQuery("select count(m) " +
+                        "from Member m " +
+                        "where m.age = :age", Long.class)
+                .setParameter("age", age)
+                .getSingleResult();
+    }
+
 }
